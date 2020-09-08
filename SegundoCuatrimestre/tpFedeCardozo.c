352a88d4197aac8187f8a6de9530e6ca62ac2db2
FedericoCardozo <fedecardozo.a@gmail.com>
2020-09-08 20:24:10 -0300

    Tp recursividad falta completar

---
+++ SegundoCuatrimestre/tpFedeCardozo.c
@@ -0,0 +1,27 @@
+
+# include <stdio.h>
+
+int recursiva(int x);
+
+int main(){
+	
+	/*Realice una función recursiva que sume los primeros n números positivos,
+	 teniendo en cuenta que suma(n) = 1 (si n = 1) y que suma(n) = n + suma(n-1) (si n > 1)*/
+	 
+	 
+	
+}
+
+int recursiva(int x){
+	
+	int suma = 1;
+	
+	if(suma == 1){
+		
+		suma = x + suma - 1;
+		
+		
+	}
+	
+	
+}
