1049f2639ecf2c3f19d9d0376ae5f5482861c58e
FedericoCardozo <fedecardozo.a@gmail.com>
2020-09-08 22:27:15 -0300

    Tp terminado

--- SegundoCuatrimestre/tpFedeCardozo.c
+++ SegundoCuatrimestre/tpFedeCardozo.c
@@ -8,20 +8,34 @@ int main(){
 	/*Realice una función recursiva que sume los primeros n números positivos,
 	 teniendo en cuenta que suma(n) = 1 (si n = 1) y que suma(n) = n + suma(n-1) (si n > 1)*/
 	 
+	 int num,res;
 	 
+	 printf("Ingrese un numero: ");
+	 scanf("%i", &num);
+	 
+	 res = recursiva(num);
+	
+	printf("El resultado es: %i", res);
 	
 }
 
 int recursiva(int x){
 	
-	int suma = 1;
+	int suma;
 	
-	if(suma == 1){
+	if(x > 1){
 		
-		suma = x + suma - 1;
+		printf("%i + ",x);
+		suma = x + recursiva(x-1);
 		
+	}else{
+		
+		suma = 1;
+		printf("%i\n",suma);
 		
 	}
+	
+	return suma;
 	
 	
 }
